423c3f3e7e035aa1c7ca0f0af1522028f7618357
Tharun Raj <tharunraj2262004@gmail.com>
2023-09-15 15:04:35 +0530

    tharun-profile

--- screen/profilescreen.js
+++ screen/profilescreen.js
@@ -11,17 +11,52 @@ import {
 import Text from "../fonts/Text";
 import TextB from "../fonts/TextBold";
 import React, { useState } from "react";
+import { useEffect } from "react";
+import axiosInstance from "../api/api";
 const { width, height } = Dimensions.get("window");
 const Profile = () => {
   const [isEditing, setIsEditing] = useState(false);
-  const [name, setName] = useState("Rahul V");
-  const [email, setEmail] = useState("rahulv@gmail.com");
-  const [address, setAddress] = useState(
-    "Plot No.14, Door No.3/2208, Padmavathy Street"
-  );
-  const [city, setCity] = useState("Chennai");
-  const [state, setState] = useState("Tamil Nadu");
+  const [name, setName] = useState("Enter your name");
+  const [email, setEmail] = useState("Enter your Email");
+  const [address, setAddress] = useState("");
+  const [addressLine1, setAddressLine1] = useState("Enter AddressLine1");
+  const [addressLine2, setAddressLine2] = useState("Enter AddressLine2");
+  const [pinCode, setPinCode] = useState("Enter Pin Code");
+  const [city, setCity] = useState("Enter City");
+  const [state, setState] = useState("Enter State");
   const [dob, setDOB] = useState("22-06-2004");
+  const [isProfileData, setProfileData] = useState([]);
+
+  useEffect(() => {
+    getProfile();
+  }, []);
+
+  const getProfile = () => {
+    axiosInstance
+      .get(`/user/view-profile`, {
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJlMThhZDY4Zi0wODgzLTRhMWUtYTk2OC05YjhjY2UwMzZjMWQiLCJpYXQiOjE2OTQ2NjY0MTAsImV4cCI6MTY5NzI1ODQxMH0.2cAX_tSAb-hUalbhi-do0GX9r5gCGK3vFQDEANtM5LFLLdpojuYFZSVKzo_Mx3L5ttEqweGlua_MFcprr0o5Zg",
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((response) => {
+        setProfileData(response.data);
+        setName(response.data.name);
+        setEmail(response.data.email);
+        setAddressLine1(response.data.address.addressLine1);
+        setAddressLine2(response.data.address.addressLine2);
+        setPinCode(response.data.address.pinCode);
+        setCity(response.data.address.district);
+        setState(response.data.address.state);
+        console.log(response.data.name);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+        console.log("hjjji");
+      });
+  };
 
   const handleEditProfile = () => {
     setIsEditing(!isEditing);
@@ -29,9 +64,36 @@ const Profile = () => {
 
   const handleUpdateProfile = () => {
     setIsEditing(false);
+    handleUpload();
   };
 
-  
+  const handleUpload = async () => {
+    const data = new FormData();
+    data.append("name", name);
+    data.append("email", email);
+    data.append("address.addressLine1", addressLine1);
+    data.append("address.addressLine2", addressLine2);
+    data.append("address.pinCode", pinCode);
+    data.append("address.district", city);
+    data.append("address.state", state);
+    data.append("address.country", "India");
+
+    axiosInstance
+      .put("/user/update-profile", data, {
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJlMThhZDY4Zi0wODgzLTRhMWUtYTk2OC05YjhjY2UwMzZjMWQiLCJpYXQiOjE2OTQ2NjY0MTAsImV4cCI6MTY5NzI1ODQxMH0.2cAX_tSAb-hUalbhi-do0GX9r5gCGK3vFQDEANtM5LFLLdpojuYFZSVKzo_Mx3L5ttEqweGlua_MFcprr0o5Zg",
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((response) => {
+        console.log(JSON.stringify(response.data));
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   return (
     <View style={styles.container}>
       <StatusBar
@@ -159,35 +221,101 @@ const Profile = () => {
               <Text style={{ color: "white", fontSize: 16 }}>{email}</Text>
             )}
           </View>
-          <View
-            style={{
-              height: height * 0.08,
-              width: width * 0.85,
-              borderBottomWidth: 1,
-              borderColor: "white",
-              justifyContent: "space-between",
-              marginBottom: "3%",
-            }}
-          >
-            <Text style={{ color: "#005f48", fontSize: 20 }}>
-              Permanent Address
-            </Text>
-            {isEditing ? (
-              <TextInput
+          {isEditing ? (
+            <View
+              style={{
+                height: height * 0.2,
+                width: width * 0.85,
+                justifyContent: "space-between",
+                marginBottom: "5%",
+              }}
+            >
+              <View style={{ height: height * 0.04 }}>
+                <Text style={{ color: "#005f48", fontSize: 20 }}>
+                  Permanent Address
+                </Text>
+              </View>
+              <View
                 style={{
-                  color: "white",
-                  fontSize: 16,
-                  fontFamily: "Montserrat",
+                  height: height * 0.04,
+                  width: width * 0.85,
+                  borderWidth: 1,
+                  borderColor: "white",
+                  borderRadius: 10,
+                  paddingHorizontal: "2%",
                 }}
-                value={address}
-                onChangeText={setAddress}
-              />
-            ) : (
-              <Text style={{ color: "white", fontSize: 16 }} numberOfLines={1}>
-                {address}
+              >
+                <TextInput
+                  style={{
+                    color: "white",
+                    fontSize: 16,
+                    fontFamily: "Montserrat",
+                  }}
+                  value={addressLine1}
+                  onChangeText={setAddressLine1}
+                />
+              </View>
+              <View
+                style={{
+                  height: height * 0.04,
+                  width: width * 0.85,
+                  borderWidth: 1,
+                  borderColor: "white",
+                  borderRadius: 10,
+                  paddingHorizontal: "2%",
+                }}
+              >
+                <TextInput
+                  style={{
+                    color: "white",
+                    fontSize: 16,
+                    fontFamily: "Montserrat",
+                  }}
+                  value={addressLine2}
+                  onChangeText={setAddressLine2}
+                />
+              </View>
+              <View
+                style={{
+                  height: height * 0.04,
+                  width: width * 0.85,
+                  borderWidth: 1,
+                  borderColor: "white",
+                  borderRadius: 10,
+                  paddingHorizontal: "2%",
+                }}
+              >
+                <TextInput
+                  style={{
+                    color: "white",
+                    fontSize: 16,
+                    fontFamily: "Montserrat",
+                  }}
+                  value={pinCode}
+                  onChangeText={setPinCode}
+                />
+              </View>
+            </View>
+          ) : (
+            <View
+              style={{
+                height: height * 0.08,
+                width: width * 0.85,
+                borderBottomWidth: 1,
+                borderColor: "white",
+                justifyContent: "space-between",
+                marginBottom: "3%",
+              }}
+            >
+              <Text style={{ color: "#005f48", fontSize: 20 }}>
+                Permanent Address
               </Text>
-            )}
-          </View>
+              <Text style={{ color: "white", fontSize: 16 }} numberOfLines={1}>
+                {addressLine1 + ", " + addressLine2 + ", " + pinCode}
+              </Text>
+            </View>
+          )}
+
           <View
             style={{
               height: height * 0.05,
